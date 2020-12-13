117f03fc88f33b6963e8911af0303d1bf3d408eb
Andrés Bórquez <andres.borquezc@gmail.com>
2020-12-13 00:23:20 -0300

    GetSimpleTuple lightweight version using less output variables

--- include/GSTtree.h
+++ include/GSTtree.h
@@ -21,14 +21,16 @@ struct data_e {
   Float_t Q2, W, Nu, Xb, Yb;
   Float_t vxe, vye, vze;
   Float_t Sector;
-  Float_t Px, Py, Pz, P;
-  Float_t Betta, Etot, Ein, Eout;
+  Float_t Px, Py, Pz;
+  Float_t Betta;
   Float_t vxec, vyec, vzec;
   Float_t TargType;
-  Float_t XEC, YEC, ZEC;
   Float_t ThetaLab, PhiLab;
-  Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status;
-  Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC;
+  // Float_t P; // optional
+  // Float_t Etot, Ein, Eout; // optional
+  // Float_t XEC, YEC, ZEC; // optional
+  // Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status; // optional
+  // Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC; // optional
   // evnt (1)
   Float_t evnt;
 };
@@ -39,25 +41,30 @@ struct data_p {
   Float_t Q2, W, Nu, Xb, Yb;
   Float_t vxe, vye, vze;
   Float_t SectorEl;
-  Float_t Pex, Pey, Pez, Pe;
-  Float_t BettaEl, Etote, Eine, Eoute;
+  Float_t Pex, Pey, Pez;
+  Float_t BettaEl;
   Float_t vxec, vyec, vzec;
   Float_t TargType;
-  Float_t XECe, YECe, ZECe;
   Float_t ThetaLabEl, PhiLabEl;
-  Float_t StatDCEl, DCStatusEl, StatECEl, ECStatusEl, TimeECEl, PathECEl, Chi2ECEl, StatSCEl, SCStatusEl, TimeSCEl, PathSCEl, StatCCEl, CCStatusEl, NpheEl, Chi2CCEl, StatusEl;
-  Float_t NRowsDCEl, NRowsECEl, NRowsSCEl, NRowsCCEl;
+  // Float_t Pe; // optional
+  // Float_t Etote, Eine, Eoute; // optional
+  // Float_t XECe, YECe, ZECe; // optional
+  // Float_t StatDCEl, DCStatusEl, StatECEl, ECStatusEl, TimeECEl, PathECEl, Chi2ECEl, StatSCEl, SCStatusEl, TimeSCEl, PathSCEl, StatCCEl, CCStatusEl, NpheEl, Chi2CCEl, StatusEl; // optional
+  // Float_t NRowsDCEl, NRowsECEl, NRowsSCEl, NRowsCCEl; // optional
   // particle (49)
-  Float_t Eh, Zh, ThetaPQ, Pt2, Pl2, PhiPQ, Mx2, T;
+  Float_t Eh, Zh, ThetaPQ, Pt2, Pl2, PhiPQ, Mx2;
   Float_t ThetaLab, PhiLab;
   Float_t vxh, vyh, vzh;
   Float_t Sector;
-  Float_t Px, Py, Pz, P;
-  Float_t Betta, Mass2, Etot, Ein, Eout;
-  Float_t XEC, YEC, ZEC;
+  Float_t Px, Py, Pz;
   Float_t pid, T4, deltaZ;
-  Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status;
-  Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC;
+  Float_t Betta, Mass2;
+  // Float_t P; // optional
+  // Float_t T; // optional
+  // Float_t Etot, Ein, Eout; // optional
+  // Float_t XEC, YEC, ZEC; // optional
+  // Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status; // optional
+  // Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC; // optional
   // event (1)
   Float_t evnt;
 };
@@ -69,13 +76,15 @@ struct sim_e {
   Float_t vxe, vye, vze;
   Float_t Sector;
   Float_t TargType;
-  Float_t Px, Py, Pz, P;
-  Float_t Betta, Etot, Ein, Eout;
+  Float_t Px, Py, Pz;
+  Float_t Betta;
   Float_t vxec, vyec, vzec;
-  Float_t XEC, YEC, ZEC;
   Float_t ThetaLab, PhiLab;
-  Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status;
-  Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC;
+  // Float_t P; // optional
+  // Float_t Etot, Ein, Eout; // optional
+  // Float_t XEC, YEC, ZEC; // optional
+  // Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status; // optional
+  // Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC; // optional
   // event (1)
   Float_t evnt;
   // gsim (17)
@@ -83,9 +92,10 @@ struct sim_e {
   Float_t mc_vxe, mc_vye, mc_vze;
   Float_t mc_Sector;
   Float_t mc_TargType;
-  Float_t mc_Px, mc_Py, mc_Pz, mc_P;
+  Float_t mc_Px, mc_Py, mc_Pz;
   Float_t mc_Betta;
   Float_t mc_ThetaLab, mc_PhiLab;
+  // Float_t mc_P; // optional
 };
 
 struct sim_p {
@@ -94,25 +104,30 @@ struct sim_p {
   Float_t Q2, W, Nu, Xb, Yb;
   Float_t vxe, vye, vze;
   Float_t SectorEl, TargType;
-  Float_t Pex, Pey, Pez, Pe;
-  Float_t BettaEl, Etote, Eine, Eoute;
+  Float_t Pex, Pey, Pez;
+  Float_t BettaEl;
   Float_t vxec, vyec, vzec;
-  Float_t XECe, YECe, ZECe;
   Float_t ThetaLabEl, PhiLabEl;
-  Float_t StatDCEl, DCStatusEl, StatECEl, ECStatusEl, TimeECEl, PathECEl, Chi2ECEl, StatSCEl, SCStatusEl, TimeSCEl, PathSCEl, StatCCEl, CCStatusEl, NpheEl, Chi2CCEl, StatusEl;
-  Float_t NRowsDCEl, NRowsECEl, NRowsSCEl, NRowsCCEl;
+  // Float_t Pe; // optional
+  // Float_t Etote, Eine, Eoute; // optional
+  // Float_t XECe, YECe, ZECe; // optional
+  // Float_t StatDCEl, DCStatusEl, StatECEl, ECStatusEl, TimeECEl, PathECEl, Chi2ECEl, StatSCEl, SCStatusEl, TimeSCEl, PathSCEl, StatCCEl, CCStatusEl, NpheEl, Chi2CCEl, StatusEl; // optional
+  // Float_t NRowsDCEl, NRowsECEl, NRowsSCEl, NRowsCCEl; // optional
   // simrec particle (49)
-  Float_t Eh, Zh, ThetaPQ, Pt2, Pl2, PhiPQ, Mx2, T;
+  Float_t Eh, Zh, ThetaPQ, Pt2, Pl2, PhiPQ, Mx2;
   Float_t ThetaLab, PhiLab;
   Float_t vxh, vyh, vzh;
   Float_t Sector;
-  Float_t Px, Py, Pz, P;
-  Float_t Betta, Mass2, Etot, Ein, Eout;
-  Float_t XEC, YEC, ZEC;
+  Float_t Px, Py, Pz;
+  Float_t Betta, Mass2;
   Float_t pid;
   Float_t T4, deltaZ;
-  Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status;
-  Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC;
+  // Float_t T; // optional
+  // Float_t P; // optional
+  // Float_t Etot, Ein, Eout; // optional
+  // Float_t XEC, YEC, ZEC; // optional
+  // Float_t StatDC, DCStatus, StatEC, ECStatus, TimeEC, PathEC, Chi2EC, StatSC, SCStatus, TimeSC, PathSC, StatCC, CCStatus, Nphe, Chi2CC, Status; // optional
+  // Float_t NRowsDC, NRowsEC, NRowsSC, NRowsCC; // optional
   // event-related (1)
   Float_t evnt;
   // gsim electron (17)
@@ -120,19 +135,22 @@ struct sim_p {
   Float_t mc_vxe, mc_vye, mc_vze;
   Float_t mc_SectorEl;
   Float_t mc_TargType;
-  Float_t mc_Pex, mc_Pey, mc_Pez, mc_Pe;
+  Float_t mc_Pex, mc_Pey, mc_Pez;
   Float_t mc_BettaEl;
   Float_t mc_ThetaLabEl, mc_PhiLabEl;
+  // Float_t mc_Pe; // optional
   // gsim particle (22)
   Float_t mc_Eh, mc_Zh, mc_ThetaPQ, mc_Pt2, mc_Pl2, mc_PhiPQ;
   Float_t mc_ThetaLab, mc_PhiLab;
-  Float_t mc_Mx2, mc_T;
+  Float_t mc_Mx2;
   Float_t mc_vxh, mc_vyh, mc_vzh;
   Float_t mc_Sector;
-  Float_t mc_Px, mc_Py, mc_Pz, mc_P;
+  Float_t mc_Px, mc_Py, mc_Pz;
   Float_t mc_Betta;
   Float_t mc_Mass2, mc_deltaZ;
   Float_t mc_pid;
+  // Float_t mc_T; // optional
+  // Float_t mc_P; // optional
 };
 
 #endif
